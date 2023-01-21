28c2d87d049b994892a369ec4a97fdde8ccbbcd7
d.buryak <d.buryak@gmaail.com>
2023-01-21 20:35:53 +0200

    added Key fetures displaing to item

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
 module ItemsHelper
+
+  def item_property_values(item)
+    item.property_values
+  end
+
+  def key_features_values(item)
+    # item.property_values.joins(:property).where(properties: Property.key_features)
+    item.property_values.where(property_id: Property.key_features.ids)
+  end
+
 end

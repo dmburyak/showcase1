15f56a5edfcba742c477202928c6d40dc2cc7714
d.buryak <d.buryak@gmaail.com>
2023-01-22 20:26:15 +0200

    testing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,7 +45,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :url, :seller_id, property_value_ids: [])
+    params.require(:item).permit(:name, :description, :price, :url, :seller_id, :property[])
     # params.require(:item).permit(:name, :description, :price, :url, :seller_id)
   end
 end

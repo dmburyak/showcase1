076bc31e45bf58501d103767357c8a9fb6ddd365
d.buryak <d.buryak@gmaail.com>
2023-03-03 20:10:04 +0200

    adding models

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -11,33 +11,41 @@ class ModelsController < ApplicationController
       main_link = params[:main_link]
 
       uri = URI.parse(main_link.to_s)
-      base_url = uri.scheme + '://' + uri.host
-
+      base_url = "#{uri.scheme}://#{uri.host}"
+      
       doc = Nokogiri::HTML(URI.open(main_link))
+                    .xpath("//script[@id='__NEXT_DATA__']").to_s
+                    .sub('<script id="__NEXT_DATA__" type="application/json">', '')
+                    .sub('</script>', '')
 
-      xpath = "//script[@id='__NEXT_DATA__']"
-      doc2 = doc.xpath(xpath).to_s
+      doc = JSON.parse(doc)
+      doc = doc['props']['pageProps']['initialDeviceList']
 
-      doc3 = doc2.sub('<script id="__NEXT_DATA__" type="application/json">', "")
-      doc4 = doc3.sub('</script>', "")
-      doc5 = JSON.parse(doc4)
-      doc6 = doc5['props']['pageProps']['initialDeviceList'][0]
+      doc&.each do |model|
 
-      item = {
-        brand: doc6['brand'],
-        name: doc6['model'],
-        base_url: base_url,
-        model_url: doc6['PDPPageURL'][0]
-      }
+        item = {
+          brand: model['brand'],
+          name: model['model'],
+          base_url: base_url,
+          model_url: model['PDPPageURL'][0]
+        }
 
-      @main_page = item
-      @model = @seller.models.new(item)
-      @model.save
+        @model = @seller.models.find_by(name: item[:name])
+
+        if @model.nil?
+          @seller.models.create(item)
+        else
+          @model.update(item)
+        end
+
+      end
+
+      @main_page = main_link
+
     end
 
-    # redirect_to seller_goods_path @seller
+    redirect_to seller_goods_path @seller
 
-    render 'sellers/goods', status: :accepted
   end
 
 end
